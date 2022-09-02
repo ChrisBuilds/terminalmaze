1a34102ae5b2c3be5a2d8815190ddf946270012b
ChrisH <741258+git@pm.me>
2022-09-01 20:18:33 -0400

    added ability to see algorithm logic

--- mazer.py
+++ mazer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from audioop import add
 from aldousbroder import AldousBroder
 from binary_tree import BinaryTree
 from sidewinder import Sidewinder
@@ -9,6 +10,8 @@ from wilsons import Wilsons
 from recursivebacktracker import RecursiveBacktracker
 from grid import Grid
 from os import system
+import colorama
+from time import sleep
 
 
 def gen_mask(mask):
@@ -29,24 +32,55 @@ def main():
     ..........
     ..........
     """
-
-    grid = Grid(20, 10, mask=gen_mask(mask))
+    colorama.init()
+    showlogic = True
+    grid = Grid(40, 20, mask=gen_mask(mask))
     # maze = BinaryTree(grid)
     # maze = Sidewinder(grid)
     # maze = AldousBroder(grid)
     # maze = Wilsons(grid)
     # maze = HuntandKill(grid)
-    algo = RecursiveBacktracker(grid)
+    algo = RecursiveBacktracker(grid, showlogic=showlogic)
 
     for maze in algo.generate_maze():
-        show_maze(maze)
+        show_maze(maze, algo.logic_data, showlogic)
 
 
-def show_maze(maze):
+def add_logic_data(visual_grid, logic_data):
+    def translate_cell_coords(cell):
+        y = cell.row
+        x = cell.column
+        if y == 0:
+            y = 1
+        else:
+            y = (y * 2) + 1
+        if x == 0:
+            x = 1
+        else:
+            x = (x * 2) + 1
+        return (y, x)
+
+    working_cell = logic_data["working_cell"]
+    last_linked = logic_data["last_linked"]
+    # color working cell
+    y, x = translate_cell_coords(working_cell)
+    visual_grid[y][x] = f"{colorama.Fore.CYAN}{chr(9608)}{colorama.Fore.RESET}"
+    # color last linked cell
+    if last_linked:
+        y, x = translate_cell_coords(last_linked)
+        visual_grid[y][x] = f"{colorama.Fore.LIGHTYELLOW_EX}{chr(9608)}{colorama.Fore.RESET}"
+    return visual_grid
+
+
+def show_maze(maze, logic_data, showlogic):
     visual_grid = maze.get_visual_grid()
+    if showlogic:
+        if logic_data["working_cell"]:
+            visual_grid = add_logic_data(visual_grid, logic_data)
     lines = ["".join(line) for line in visual_grid]
     system("clear")
     print("\n".join(lines))
+    # sleep(0.05)
 
 
 if __name__ == "__main__":
--- recursivebacktracker.py
+++ recursivebacktracker.py
@@ -3,18 +3,22 @@ import random
 
 
 class RecursiveBacktracker:
-    def __init__(self, grid: Grid):
+    def __init__(self, grid: Grid, showlogic=False):
         self.grid = grid
+        self.showlogic = showlogic
+        self.logic_data = {"working_cell": None, "last_linked": None}
 
     def generate_maze(self):
         cell = self.grid.random_cell()
         stack = []
         stack.append(cell)
         while stack:
+            self.logic_data["working_cell"] = cell
             unvisited_neighbors = [c for c in self.grid.get_neighbors(cell) if not c.links]
             if unvisited_neighbors:
                 next_cell = random.choice(unvisited_neighbors)
                 cell.link(next_cell)
+                self.logic_data["last_linked"] = next_cell
                 stack.append(next_cell)
                 cell = next_cell
                 yield self.grid
@@ -23,3 +27,6 @@ class RecursiveBacktracker:
                 stack.pop()
                 if stack:
                     cell = stack[-1]
+                    if self.showlogic:
+                        self.logic_data["working_cell"] = cell
+                        yield self.grid
