d0cdb675e576872ef67170043cade19f18b4fe23
ChrisH <741258+git@pm.me>
2022-09-05 00:33:57 -0400

    send status text to show_maze

--- terminalmaze.py
+++ terminalmaze.py
@@ -14,7 +14,7 @@ import sys
 
 
 def main():
-    mask = """
+    python = """
 ##  ############################################################
 ##                                                            ##  
 ##  ########  ##    ## ######## ##     ##  #######  ##    ##  ##
@@ -27,7 +27,7 @@ def main():
 ##                                                            ##
 ############################################################  ##
     """
-    mask = """
+    amanda = """
     ######  ########     ###     ######  ########          
    ##    ##        ##   ## ##   ##    ## ##                
    ##       ##                  ##       ##                
@@ -45,23 +45,24 @@ def main():
 ##     ## ##     ## ##     ## ##    ## ########  ##     ## 
     """
 
-    showlogic = True
-    maze = Grid(100, 27, mask=mask)
-    # algo = BinaryTree(maze, showlogic=showlogic)
-    # algo = Sidewinder(maze, showlogic=showlogic)
-    # algo = AldousBroder(maze, showlogic=showlogic)
-    # algo = Wilsons(maze, showlogic=showlogic)
-    # algo = HuntandKill(maze, showlogic=showlogic)
-    algo = RecursiveBacktracker(maze, showlogic=showlogic)
-    solver = BreadthFirst(maze)
+    show_mazelogic = True
+    show_solvelogic = True
+    maze = Grid(100, 27, mask=amanda)
+    # algo = BinaryTree(maze, showlogic=show_mazelogic)
+    # algo = Sidewinder(maze, showlogic=show_mazelogic)
+    # algo = AldousBroder(maze, showlogic=show_mazelogic)
+    # algo = Wilsons(maze, showlogic=show_mazelogic)
+    # algo = HuntandKill(maze, showlogic=show_mazelogic)
+    algo = RecursiveBacktracker(maze, showlogic=show_mazelogic)
+    solver = BreadthFirst(maze, showlogic=show_solvelogic)
     try:
         maze: Grid
         for maze in algo.generate_maze():
-            maze.visual.show(algo.logic_data, showlogic)
-            sleep(0.0125)
+            maze.visual.show(algo.logic_data, algo.status_text, show_mazelogic)
+            sleep(0.0135)
         for maze in solver.solve():
-            maze.visual.show(solver.logic_data, showlogic)
-            sleep(0.0125)
+            maze.visual.show(solver.logic_data, solver.status_text, show_solvelogic)
+            sleep(0.0135)
     except KeyboardInterrupt:
         print("Maze generation stopped.")
         sys.exit()
