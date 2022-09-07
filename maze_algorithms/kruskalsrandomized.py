575ed6cdb0db42ce9eedbd574556f8d256935621
ChrisH <741258+git@pm.me>
2022-09-07 12:54:46 -0400

    larger groups absorb and added colored groups

--- maze_algorithms/kruskalsrandomized.py
+++ maze_algorithms/kruskalsrandomized.py
@@ -1,5 +1,7 @@
+from cgitb import small
 from grid.grid import Grid, Cell
 import random
+from collections import defaultdict
 from typing import Union
 
 
@@ -8,21 +10,20 @@ class KruskalsRandomized:
         self.maze: Grid = maze
         self.showlogic: bool = showlogic
         self.logic_data: dict[str, Cell] = {}
+        self.group_to_cell_map_logic: defaultdict[int, list[Cell]] = defaultdict(list)  # group : [cells]
+        self.cell_to_group_map: dict[tuple[int, int], int] = {}  # cell_address : group
+        self.logic_data["groups"] = self.group_to_cell_map_logic
         self.status_text: dict[str, Union[str, int]] = {"Algorithm": "Kruskal's Randomized"}
 
     def generate_maze(self) -> Grid:
-        group_a_logic = list()
-        group_b_logic = list()
-        self.logic_data["logic1"] = group_a_logic
-        self.logic_data["logic0"] = group_b_logic
         links: list[tuple[Cell, Cell]] = list()
         groups = list()
-        group_map = {}  # cell_address : group
         group_id = 0
         for cell in self.maze.each_cell():
             # initialize groups
             groups.append(group_id)
-            group_map[(cell.row, cell.column)] = group_id
+            self.cell_to_group_map[(cell.row, cell.column)] = group_id
+            # group_to_cell_map_logic[group_id].append(cell)
             group_id += 1
             # find all links
             for neighbor in self.maze.get_neighbors(cell).values():
@@ -33,27 +34,36 @@ class KruskalsRandomized:
             link = links.pop(random.randrange(len(links)))
             cell_a = link[0]
             cell_a_address = (cell_a.row, cell_a.column)
-            cell_a_group = group_map[cell_a_address]
+            cell_a_group = self.cell_to_group_map[cell_a_address]
             cell_b = link[1]
             cell_b_address = (cell_b.row, cell_b.column)
-            cell_b_group = group_map[cell_b_address]
+            cell_b_group = self.cell_to_group_map[cell_b_address]
+            if cell_a not in self.group_to_cell_map_logic[cell_a_group]:
+                self.group_to_cell_map_logic[cell_a_group].append(cell_a)
+            if cell_b not in self.group_to_cell_map_logic[cell_b_group]:
+                self.group_to_cell_map_logic[cell_b_group].append(cell_b)
             if cell_a_group == cell_b_group:
                 continue
             self.maze.link_cells(cell_a, cell_b)
-            groups.remove(cell_b_group)
-            for cell, group in group_map.items():
-                if group == cell_a_group:
-                    group_a_logic.append(self.maze.get_cell(cell))
-                if group == cell_b_group:
-                    group_b_logic.append(self.maze.get_cell(cell))
-                    group_map[cell] = cell_a_group
-                # if self.showlogic:
-                #     if group in (cell_a_group, cell_b_group):
-                #        if len(group_b_logic) > 20 and len(group_a_logic) > 20:
-                #            yield self.maze
-            group_a_logic.clear()
-            group_b_logic.clear()
+
+            self.merge_groups(cell_a, cell_b, groups)
 
             self.status_text["Available Links"] = len(links)
             self.status_text["Groups"] = len(groups)
             yield self.maze
+
+    def merge_groups(self, cell_a: Cell, cell_b: Cell, groups):
+        cell_a_group = self.cell_to_group_map[(cell_a.row, cell_a.column)]
+        cell_b_group = self.cell_to_group_map[(cell_b.row, cell_b.column)]
+        smaller_group, larger_group = sorted(
+            [cell_a_group, cell_b_group], key=lambda group: len(self.group_to_cell_map_logic[group])
+        )
+        self.group_to_cell_map_logic[larger_group].extend(self.group_to_cell_map_logic[smaller_group])
+        del self.group_to_cell_map_logic[smaller_group]
+        for cell, group in self.cell_to_group_map.items():
+            if group == smaller_group:
+                if cell_a_group == larger_group:
+                    self.cell_to_group_map[cell] = cell_a_group
+                elif cell_b_group == larger_group:
+                    self.cell_to_group_map[cell] = cell_b_group
+        groups.remove(smaller_group)
