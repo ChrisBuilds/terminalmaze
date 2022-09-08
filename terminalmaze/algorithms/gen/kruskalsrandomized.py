8261fe8c7f47008e96f91ec139066557234bc46d
ChrisH <741258+git@pm.me>
2022-09-07 23:37:56 -0400

    changed lists to sets now that cells are hashable

--- terminalmaze/algorithms/gen/kruskalsrandomized.py
+++ terminalmaze/algorithms/gen/kruskalsrandomized.py
@@ -1,5 +1,5 @@
-from resources.grid import Grid
-from resources.cell import Cell
+from terminalmaze.resources.grid import Grid
+from terminalmaze.resources.cell import Cell
 import random
 from collections import defaultdict
 from typing import Union
@@ -10,39 +10,37 @@ class KruskalsRandomized:
         self.maze: Grid = maze
         self.showlogic: bool = showlogic
         self.logic_data: dict[str, Cell] = {}
-        self.group_to_cell_map_logic: defaultdict[int, list[Cell]] = defaultdict(list)  # group : [cells]
-        self.cell_to_group_map: dict[tuple[int, int], int] = {}  # cell_address : group
+        self.group_to_cell_map_logic: defaultdict[int, set[Cell]] = defaultdict(set)  # group : {cells}
+        self.cell_to_group_map: dict[Cell, int] = {}  # cell_address : group
         self.logic_data["groups"] = self.group_to_cell_map_logic
         self.status_text: dict[str, Union[str, int]] = {"Algorithm": "Kruskal's Randomized", "Seed": self.maze.seed}
         random.seed(self.maze.seed)
 
     def generate_maze(self) -> Grid:
-        links: list[tuple[Cell, Cell]] = list()
+        links: set[tuple[Cell, Cell]] = set()
         groups = list()
         group_id = 0
         for cell in self.maze.each_cell():
             # initialize groups
             groups.append(group_id)
-            self.cell_to_group_map[(cell.row, cell.column)] = group_id
+            self.cell_to_group_map[cell] = group_id
             # group_to_cell_map_logic[group_id].append(cell)
             group_id += 1
             # find all links
             for neighbor in self.maze.get_neighbors(cell).values():
                 if (neighbor, cell) not in links:  # check if reversed link is already in list
-                    links.append((cell, neighbor))
+                    links.add((cell, neighbor))
 
         while len(groups) > 1:
-            link = links.pop(random.randrange(len(links)))
+            link = links.pop()
             cell_a = link[0]
-            cell_a_address = (cell_a.row, cell_a.column)
-            cell_a_group = self.cell_to_group_map[cell_a_address]
+            cell_a_group = self.cell_to_group_map[cell_a]
             cell_b = link[1]
-            cell_b_address = (cell_b.row, cell_b.column)
-            cell_b_group = self.cell_to_group_map[cell_b_address]
+            cell_b_group = self.cell_to_group_map[cell_b]
             if cell_a not in self.group_to_cell_map_logic[cell_a_group]:
-                self.group_to_cell_map_logic[cell_a_group].append(cell_a)
+                self.group_to_cell_map_logic[cell_a_group].add(cell_a)
             if cell_b not in self.group_to_cell_map_logic[cell_b_group]:
-                self.group_to_cell_map_logic[cell_b_group].append(cell_b)
+                self.group_to_cell_map_logic[cell_b_group].add(cell_b)
             if cell_a_group == cell_b_group:
                 continue
             self.maze.link_cells(cell_a, cell_b)
@@ -54,12 +52,12 @@ class KruskalsRandomized:
             yield self.maze
 
     def merge_groups(self, cell_a: Cell, cell_b: Cell, groups):
-        cell_a_group = self.cell_to_group_map[(cell_a.row, cell_a.column)]
-        cell_b_group = self.cell_to_group_map[(cell_b.row, cell_b.column)]
+        cell_a_group = self.cell_to_group_map[cell_a]
+        cell_b_group = self.cell_to_group_map[cell_b]
         smaller_group, larger_group = sorted(
             [cell_a_group, cell_b_group], key=lambda group: len(self.group_to_cell_map_logic[group])
         )
-        self.group_to_cell_map_logic[larger_group].extend(self.group_to_cell_map_logic[smaller_group])
+        self.group_to_cell_map_logic[larger_group].update(self.group_to_cell_map_logic[smaller_group])
         del self.group_to_cell_map_logic[smaller_group]
         for cell, group in self.cell_to_group_map.items():
             if group == smaller_group:
