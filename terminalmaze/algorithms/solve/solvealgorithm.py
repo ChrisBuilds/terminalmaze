4b11100a3e9eab42275e5570011cd6c6890a43a8
ChrisH <741258+git@pm.me>
2022-09-11 21:59:56 -0400

    Add time_elapsed method to get running time

--- terminalmaze/algorithms/solve/solvealgorithm.py
+++ terminalmaze/algorithms/solve/solvealgorithm.py
@@ -1,14 +1,28 @@
 from terminalmaze.resources.grid import Grid
 from terminalmaze.tools.visualeffects import VisualEffect
+import time
 from typing import Union, Optional, Generator
 
 
 class SolveAlgorithm:
-    def __init__(self, maze: Grid, showlogic: bool = False) -> None:
+    def __init__(self, maze: Grid) -> None:
         self.maze: Grid = maze
-        self.showlogic: bool = showlogic
         self.visual_effects: dict[str, VisualEffect] = dict()
-        self.status_text: dict[str, Union[Optional[str], Optional[int]]] = {"Seed": self.maze.seed}
+        self.status_text: dict[str, Union[Optional[str], Optional[int]]]
+        self.status_text = {"Algorithm": "", "Seed": self.maze.seed, "Time Elapsed": ""}
+        self.start_time = time.time()
 
     def solve(self) -> Generator[Grid, None, None]:
         yield self.maze
+
+    def time_elapsed(self) -> str:
+        """
+        Calculate the run time in minutes/seconds and return string representation.
+        Returns
+        -------
+        str : Time in format {minutes}m {seconds}s
+        """
+        seconds = time.time() - self.start_time
+        minutes = int(seconds // 60)
+        seconds = int(seconds % 60)
+        return f"{minutes}m {seconds}s"
