74d900c0a99fa640bec985c043e232b97f533c19
ChrisH <741258+git@pm.me>
2022-09-13 20:56:37 -0400

    Support themes

--- terminalmaze/algorithms/gen/recursivebacktracker.py
+++ terminalmaze/algorithms/gen/recursivebacktracker.py
@@ -6,34 +6,40 @@ from typing import Generator
 
 
 class RecursiveBacktracker(Algorithm):
-    def __init__(self, maze: Grid) -> None:
+    def __init__(self, maze: Grid, theme: ve.Theme) -> None:
         super().__init__(maze)
         self.status_text["Algorithm"] = "Recursive Backtracker"
         self.status_text.update({"Pending Paths": "", "Stack Length": "", "State": ""})
         self.skip_frames = 2
+        self.theme = theme["recursive_backtracker"]
 
     def generate_maze(self) -> Generator[Grid, None, None]:
         cell = self.maze.random_cell()
         stack = [cell]
         backtracked: list[Cell] = list()
         stacktrail: list[Cell] = list()
-        ve_stack = ve.ColorMultipleCells(layer=0, category=ve.STYLE, color=218, cells=stack)
+        ve_stack = ve.ColorMultipleCells(
+            layer=0, category=ve.STYLE, color=self.theme["stack"], cells=stack  # type: ignore [arg-type]
+        )
         self.visual_effects["stack"] = ve_stack
-        ve_workingcell = ve.ColorSingleCell(layer=1, category=ve.LOGIC, color=218, cell=cell)
+        ve_workingcell = ve.ColorSingleCell(
+            layer=1, category=ve.LOGIC, color=self.theme["workingcell"], cell=cell  # type: ignore [arg-type]
+        )
         self.visual_effects["working_cell"] = ve_workingcell
-        ve_invalidneighbors = ve.ColorMultipleCells(layer=1, category=ve.LOGIC, color=52, cells=[])
+        ve_invalidneighbors = ve.ColorMultipleCells(
+            layer=1, category=ve.LOGIC, color=self.theme["invalidneighbors"], cells=[]  # type: ignore [arg-type]
+        )
         self.visual_effects["invalid_neighbors"] = ve_invalidneighbors
-        ve_lastlinked = ve.ColorSingleCell(layer=1, category=ve.LOGIC, color=218, cell=cell)
+        ve_lastlinked = ve.ColorSingleCell(
+            layer=1, category=ve.LOGIC, color=self.theme["lastlinked"], cell=cell  # type: ignore [arg-type]
+        )
         self.visual_effects["last_linked"] = ve_lastlinked
         ve_stacktrail = ve.TrailingColor(
-            layer=1,
-            category=ve.STYLE,
-            colors=[200, 200, 201, 201, 204, 204, 205, 205, 205, 206, 206, 207, 207],
-            cells=stacktrail,
+            layer=1, category=ve.STYLE, colors=self.theme["stacktrail"], cells=stacktrail  # type: ignore [arg-type]
         )
         self.visual_effects["stack_trail"] = ve_stacktrail
         ve_backtracktrail = ve.TrailingColor(
-            layer=2, category=ve.STYLE, colors=[224, 224, 188, 188, 158, 158, 159, 159, 86, 86, 44, 44, 6], cells=[]
+            layer=2, category=ve.STYLE, colors=self.theme["backtracktrail"], cells=[]  # type: ignore [arg-type]
         )
         self.visual_effects["backtrack_trail"] = ve_backtracktrail
         while stack:
