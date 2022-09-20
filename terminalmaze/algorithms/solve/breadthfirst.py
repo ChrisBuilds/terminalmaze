6c589481df3cb146f8bb6338b58bb32bd38625ac
ChrisH <741258+git@pm.me>
2022-09-19 22:05:09 -0400

    Add character transitions

--- terminalmaze/algorithms/solve/breadthfirst.py
+++ terminalmaze/algorithms/solve/breadthfirst.py
@@ -27,18 +27,27 @@ class BreadthFirst(Algorithm):
         )
         self.visual_effects["frontier"] = ve_frontier
         ve_visited = ve.ColorMultipleCells(
-            layer=0, category=ve.STYLE, color=self.theme["visited"], cells=visited  # type: ignore [arg-type]
+            layer=0, category=ve.LOGICSTYLE, color=self.theme["visited"], cells=visited  # type: ignore [arg-type]
         )
         self.visual_effects["visited"] = ve_visited
-        ve_visited_transition = ve.ColorTransition(
+        ve_visited_color_transition = ve.ColorTransition(
             layer=1,
             category=ve.STYLE,
-            colors=self.theme["pathtransition"],  # type: ignore [arg-type]
+            colors=self.theme["visited_color_transition"],  # type: ignore [arg-type]
             cells=[],
-            frames_per_color=10,
+            frames_per_state=10,
             transitioning=dict(),
         )
-        self.visual_effects["visited_transition"] = ve_visited_transition
+        self.visual_effects["visited_transition"] = ve_visited_color_transition
+        ve_visited_character_transition = ve.CharacterTransition(
+            layer=1,
+            category=ve.STYLE,
+            characters=self.theme["visited_character_transition"],  # type: ignore [arg-type]
+            cells=[],
+            frames_per_state=10,
+            transitioning=dict(),
+        )
+        self.visual_effects["visited_character_transition"] = ve_visited_character_transition
         ve_target = ve.ColorSingleCell(
             layer=0, category=ve.LOGIC, color=self.theme["target"], cell=target  # type: ignore [arg-type]
         )
@@ -48,16 +57,16 @@ class BreadthFirst(Algorithm):
         )
         self.visual_effects["position"] = ve_workingcell
         ve_solutionpath = ve.ColorMultipleCells(
-            layer=1, category=ve.LOGIC, color=self.theme["solutionpath"], cells=[]  # type: ignore [arg-type]
+            layer=1, category=ve.LOGICSTYLE, color=self.theme["solutionpath"], cells=[]  # type: ignore [arg-type]
         )
         self.visual_effects["path"] = ve_solutionpath
         ve_pathtransition = ve.ColorTransition(
             layer=2,
             category=ve.STYLE,
-            colors=self.theme["pathtransition"],  # type: ignore [arg-type]
+            colors=self.theme["visited_color_transition"],  # type: ignore [arg-type]
             cells=[],
             transitioning=dict(),
-            frames_per_color=1,
+            frames_per_state=1,
         )
         self.visual_effects["pathtransition"] = ve_pathtransition
         ve_solutiontransition = ve.ColorTransition(
@@ -66,7 +75,7 @@ class BreadthFirst(Algorithm):
             colors=self.theme["solutiontransition"],  # type: ignore [arg-type]
             cells=[],
             transitioning=dict(),
-            frames_per_color=1,
+            frames_per_state=1,
         )
         self.visual_effects["solutiontransition"] = ve_solutiontransition
         while frontier:
@@ -76,7 +85,8 @@ class BreadthFirst(Algorithm):
             position = frontier.pop(0)
             visited.append(position)
             if position not in transitions:
-                ve_visited_transition.cells.append(position)
+                ve_visited_color_transition.cells.append(position)
+                ve_visited_character_transition.cells.append(position)
             ve_workingcell.cell = position
             edges = [neighbor for neighbor in position.links if neighbor not in explored and neighbor not in frontier]
             for cell in edges:
