850fd5296813ab9053d76b2bb5aafac7232e2553
ChrisH <741258+git@pm.me>
2022-09-21 22:37:11 -0400

    Removes categories

--- terminalmaze/tools/visualeffects.py
+++ terminalmaze/tools/visualeffects.py
@@ -1,20 +1,16 @@
 from terminalmaze.resources.cell import Cell
 from typing import DefaultDict, Literal
-from terminalmaze.config import LayerColor, Transition
+from terminalmaze.config import LayerColor, Transition, LayerVerbosity
 
 
 GroupType = dict[int, list[Cell]] | DefaultDict[int, list[Cell]] | dict[int, set[Cell]] | DefaultDict[int, set[Cell]]
-LOGIC: Literal["logic"] = "logic"
-STYLE: Literal["style"] = "style"
-LOGICSTYLE: Literal["logicstyle"] = "logicstyle"
-Category = Literal["logic", "style", "logicstyle"]
 Theme = dict[str, dict[str, int | list[int]]]
 
 
 class Effect:
     """Apply a visual effect to the cell(s)."""
 
-    def __init__(self, category: Category, theme_data: LayerColor | Transition | int):
+    def __init__(self, theme_data: LayerColor | Transition | LayerVerbosity):
         """
 
         Args:
@@ -23,11 +19,7 @@ class Effect:
             category (ve.Category): Category of the Effect to be applied at specific verbosity
         levels.
         """
-        if isinstance(theme_data, int):
-            self.layer: int = theme_data
-        else:
-            self.layer = theme_data.layer
-        self.category: Category = category
+        self.layer = theme_data.layer
 
     def __lt__(self, other: "Effect"):
         return self.layer < other.layer
@@ -36,39 +28,43 @@ class Effect:
 class ColorSingleCell(Effect):
     """Color a single cell the given color."""
 
-    def __init__(self, category: Category, theme_data: LayerColor):
-        super().__init__(category, theme_data)
+    def __init__(self, theme_data: LayerColor):
+        super().__init__(theme_data)
         self.cell: Cell = Cell(0, 0)
         self.color: int = theme_data.color
+        self.verbosity: list[int] = theme_data.verbosity
 
 
 class ColorMultipleCells(Effect):
     """Color multiple cells the same color."""
 
-    def __init__(self, category: Category, theme_data: LayerColor):
-        super().__init__(category, theme_data)
+    def __init__(self, theme_data: LayerColor):
+        super().__init__(theme_data)
         self.cells: list[Cell] = list()
         self.color: int = theme_data.color
+        self.verbosity: list[int] = theme_data.verbosity
 
 
 class RandomColorGroup(Effect):
     """Color groups of cells with randomly chosen colors. Colors will be
     assigned to groups and persist between show() calls."""
 
-    def __init__(self, category: Category, theme_data: int):
-        super().__init__(category, theme_data)
+    def __init__(self, theme_data: LayerVerbosity):
+        super().__init__(theme_data)
         self.groups: GroupType
+        self.verbosity: list[int] = theme_data.verbosity
 
 
 class ValueTransition(Effect):
-    def __init__(self, category: Category, theme_data: Transition):
+    def __init__(self, theme_data: Transition):
         """Color cells with a transitioning colors and characters with a transition speed based on frames per color."""
-        super().__init__(category, theme_data)
+        super().__init__(theme_data)
         self.cells: list[Cell] = list()
         self.transitioning: dict[tuple[int, int], list[int]] = dict()
         self.colors: list[int] = theme_data.colors
         self.characters: list[str] = theme_data.characters
         self.frames_per_value: int = theme_data.frames_per_value
+        self.verbosity: list[int] = theme_data.verbosity
 
     def __lt__(self, other: "Effect"):
         return self.layer < other.layer
